b0d8191a04e601000a314cc87082699d346012fb
azedede <aidri5151@yahoo.com>
2022-06-11 00:47:10 -0400

    modify myIncludes: by removing if true/false

--- javascript-methods.js
+++ javascript-methods.js
@@ -1,99 +1,97 @@
 // MAP //
-Array.prototype.myMap = function(callbackFn) {
-    this.forEach((element,index) => {
-      this[index] = callbackFn(element);
-    });
-  };
-  
-  // FILTER //
-  Array.prototype.myFilter = function(callbackFn) {
-    // Place your code here.
-    let result = [];
-    this.forEach(element => {
-      if(callbackFn(element)){
-        result.push(element);
-      }
-    });
-    return result;
-  
-  };
-  
-  // SOME //
-  Array.prototype.mySome = function(callbackFn) {
-    // Place your code here.
-    this.forEach(element => {
-      if(callbackFn(element)){
-        return true;
-      }
-    });
-    return false;
-  };
-  
-  // EVERY //
-  Array.prototype.myEvery = function(callbackFn) {
-    // Place your code here.
-    this.forEach(element => {
-      if(callbackFn(element) == false){
-        return false;
-      }
-    });
-    return true;
-  };
-  
-  // REDUCE //
-  Array.prototype.myReduce = function(callbackFn, initialValue) {
-	if(initialValue != undefined) {
-		callbackFn(initialValue, this[0])
-	}
-    for(let i = 1; i < this.length; i++) {
-		callbackFn(this[i-1], this[i])
-	}
-  };
+Array.prototype.myMap = function (callbackFn) {
+  this.forEach((element, index) => {
+    this[index] = callbackFn(element);
+  });
+};
 
-  // INCLUDES //
-  Array.prototype.myIncludes = function(searchElement) {
-    for(let i = 0; i < this.length; i++) {
-		if(this[i] == searchElement) {
-			return true;
-		}
-	}
-	return false;
-  };
-  
-  // INDEXOF //
-  Array.prototype.myIndexOf = function(searchElement) {
-    for(let i = 0; i < this.length; i++) {
-		if(this[i] == searchElement) {
-			return i;
-		}
-	}
-	 
-	return -1;
-  };
-  
-  // LASTINDEXOF //
-  Array.prototype.myLastIndexOf = function(searchElement) {
-    for (let i = this.length - 1; i > -1; i--) {
-      if (this[i] == searchElement) {return i;}
+// FILTER //
+Array.prototype.myFilter = function (callbackFn) {
+  // Place your code here.
+  let result = [];
+  this.forEach(element => {
+    if (callbackFn(element)) {
+      result.push(element);
     }
-    return -1;
-  };
-  
-  // KEYS //
-  Object.myKeys = function(object) {
-    let keyArr = [];
-    for (var key in object){
-      keyArr.push(key);
+  });
+  return result;
+
+};
+
+// SOME //
+Array.prototype.mySome = function (callbackFn) {
+  // Place your code here.
+  this.forEach(element => {
+    if (callbackFn(element)) {
+      return true;
     }
-    return keyArr;
-  };
-  
-  // VALUES //
-  Object.myValues = function(object) {
-    let varArr = [];
-    for (var key in object){
-      varArr.push(object[key]);
+  });
+  return false;
+};
+
+// EVERY //
+Array.prototype.myEvery = function (callbackFn) {
+  // Place your code here.
+  this.forEach(element => {
+    if (callbackFn(element) == false) {
+      return false;
     }
-    return varArr;
-  };
-  
+  });
+  return true;
+};
+
+// REDUCE //
+Array.prototype.myReduce = function (callbackFn, initialValue) {
+  if (initialValue != undefined) {
+    callbackFn(initialValue, this[0])
+  }
+  for (let i = 1; i < this.length; i++) {
+    callbackFn(this[i - 1], this[i])
+  }
+};
+
+// INCLUDES // j
+Array.prototype.myIncludes = function (searchElement) {
+  for (let i = 0; i < this.length; i++) {
+    return (this[i] == searchElement)
+  }
+};
+
+// INDEXOF //
+Array.prototype.myIndexOf = function (searchElement) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] == searchElement) {
+      return i;
+    }
+  }
+
+  return -1;
+};
+
+// LASTINDEXOF //
+Array.prototype.myLastIndexOf = function (searchElement) {
+  for (let i = this.length - 1; i > -1; i--) {
+    if (this[i] == searchElement) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+// KEYS //
+Object.myKeys = function (object) {
+  let keyArr = [];
+  for (var key in object) {
+    keyArr.push(key);
+  }
+  return keyArr;
+};
+
+// VALUES //
+Object.myValues = function (object) {
+  let varArr = [];
+  for (var key in object) {
+    varArr.push(object[key]);
+  }
+  return varArr;
+};
